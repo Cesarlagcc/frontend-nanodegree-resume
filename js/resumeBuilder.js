0ed91cda8e59f80f1538875269d2e722cfe28c09
Cesarlagcc <cesarandon092@gmail.com>
2017-09-04 23:12:29 -0400

    updated contacts to footer

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -38,7 +38,7 @@ var bio = {
         var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
         $('#header').prepend(formattedName + formattedRole);
-        $('#topContacts').append(formattedMobile + formattedEmail +
+        $('#topContacts, #footerContacts').append(formattedMobile + formattedEmail +
             formattedGithub + formattedTwitter + formattedLocation);
         $('#header').append(formattedBioPic);
         $('#header').append(formattedWelcomeMessage);
